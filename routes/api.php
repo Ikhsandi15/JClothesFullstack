ac5d94e59b3d09cff27574a64f4b253f5a723939
Ryxena <rubbenme@proton.me>
2024-06-04 21:08:50 +0700

    Updating route

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::prefix('v1')->group(function () {
     Route::get('pesan/{id}', [PesananController::class, 'index']);
